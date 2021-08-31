5d70e83805fcd3e402ddbfe36c221688d4a115cb
ksumit1818 <ksumit1818@gmail.com>
2021-08-31 15:43:54 +0530

    initial imported blank project

---
+++ udemy-beer-main/src/main/java/com/example/udemybeermain/UdemyBeerMainApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemybeermain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyBeerMainApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyBeerMainApplication.class, args);
+	}
+
+}
---
+++ udemy-beer-main/src/test/java/com/example/udemybeermain/UdemyBeerMainApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemybeermain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyBeerMainApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
